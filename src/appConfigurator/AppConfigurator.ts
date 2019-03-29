4bbf978541b4808fd9b91233196bd9f131c98a81
André Andrade <alandrade21@gmail.com>
2019-03-29 16:01:45 -0300

    Writting documentation

--- src/appConfigurator/AppConfigurator.ts
+++ src/appConfigurator/AppConfigurator.ts
@@ -17,29 +17,29 @@ export abstract class AppConfigurator <T extends ConfigData> {
   private _appOptions: T;
 
   /**
-   * This constructor verifies which is the user's OS, and detects the user's home folder accordingly.
+   * This constructor verifies which is the user's OS, and choses the config folder accordingly.
    *
-   * If the evironment is development, the home user folder is set to the value informed in the 
+   * If the environment is development, the config folder is set to the value informed in the
    * devConfigFolderPath parameter.
    *
    * If the environment is not development and the OS is windows, the config folder is set to the
-   * folder .config inside the app instalation folder.
+   * folder .config inside the app installation folder.
    *
    * If the environment is not development and the OS is not windows (i.e. linux like OS), the config
    * folder is set to the folder .config/<<appName>>/ inside the actual OS user's home folder.
-   * 
-   * This constructor uses this informations to configure a ConfigFileManager.
-   * 
-   * @param appName Name of the app being configured. This name will be used to create a folder 
+   *
+   * This constructor uses this information to configure a ConfigFileManager.
+   *
+   * @param appName Name of the app being configured. This name will be used to create a folder
    * structure to host the configuration file.
-   * 
-   * @param devConfigFolderPath Absolute path to the development data folder structure. This structure 
-   * is a copy of the system folders that will be used during production. For more info, see 
+   *
+   * @param devConfigFolderPath Absolute path to the development data folder structure. This structure
+   * is a copy of the system folders that will be used during production. For more info, see
    * https://github.com/alandrade21/devTestFolders.
-   * 
-   * @param configFileName Name of the configuration file to be used. This file will have the .json 
+   *
+   * @param configFileName Name of the configuration file to be used. This file will have the .json
    * extension.
-   * 
+   *
    * @throws InvalidParameterError if the configFileName is empty.
    * @throws ConfigFileError if the configFileName is malformed.
    */
@@ -75,22 +75,22 @@ export abstract class AppConfigurator <T extends ConfigData> {
   }
 
   /**
-   * Method to start the app configuration. 
-   * 
-   * This superclass execute the existance verification of the config file. If it exists, the file is 
-   * read and the options object is initialized. If it not exists, the method that creates the config 
+   * Method to start the app configuration.
+   *
+   * This superclass execute the existance verification of the config file. If it exists, the file is
+   * read and the options object is initialized. If it not exists, the method that creates the config
    * file is called.
-   * 
+   *
    * This method shoud be overriden to execute specific configurations.
-   * 
+   *
    * This method show a dialog box with an error message in case of errors.
-   * 
+   *
    * @throws ConfigFileError in case of error interacting with the config file.
    */
   public doConfig(): void {
 
-    let hasFile: boolean; 
-    
+    let hasFile: boolean;
+
     try {
       hasFile = this.cfm.fileExist();
     } catch (error) {
@@ -107,7 +107,7 @@ export abstract class AppConfigurator <T extends ConfigData> {
 
   /**
    * Reads the config file and show an error message in case of error.
-   * 
+   *
    * @throws ConfigFileError in case of error interacting with the config file.
    */
   private readConfigFile(): void {
@@ -120,16 +120,16 @@ export abstract class AppConfigurator <T extends ConfigData> {
   }
 
   /**
-   * The implementation of this method should initialize an options object with its initial value and 
+   * The implementation of this method should initialize an options object with its initial value and
    * write it to the disk using the ConfigFileManager instance created by this superclass.
    */
   protected abstract createConfigFile(): void;
 
   /**
    * Show a dialog box with an error message extracted from the error parameter.
-   * 
+   *
    * This dialog uses 'Configuration Execution Error.' as title.
-   * 
+   *
    * @param error a ConfigFileError.
    */
   protected errorDialog(error: ConfigFileError): void {
@@ -138,11 +138,11 @@ export abstract class AppConfigurator <T extends ConfigData> {
 
   /**
    * Show a error dialog box with a standard message for unexpected errors.
-   * 
+   *
    * @param title to be shown in the error dilog.
    */
   protected unknowErrorDialog(title: string): void {
-    dialog.showErrorBox(title, 'An unexpected error ocurred. ' + 
+    dialog.showErrorBox(title, 'An unexpected error ocurred. ' +
                             'To see the error details, run this aplication on terminal.');
   }
 
@@ -160,4 +160,3 @@ export abstract class AppConfigurator <T extends ConfigData> {
     this._appOptions = value;
   }
 }
-
